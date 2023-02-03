72ffe675c5893dea32e82002ab1ea8437a04ce68
Mitsuaki Sato <sato@c2c-platform.com>
2023-02-03 14:27:04 +0900

    feature: Add Community dropdown component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/firebase/clientApp";
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
@@ -10,8 +11,17 @@ type NavbarProps = {};
 const Navbar: React.FC<NavbarProps> = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+      >
         <Image src="/images/redditFace.svg" height="30px" />
         <Image
           src="/images/redditText.svg"
@@ -19,7 +29,9 @@ const Navbar: React.FC<NavbarProps> = () => {
           display={{ base: "none", md: "unset" }}
         />
       </Flex>
-      <SearchInput />
+      {user && <Directory />}
+
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
   );
